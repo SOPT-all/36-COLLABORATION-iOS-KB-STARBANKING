52ba3cf150095cde20b47604d985a34a93907a1d
sem-git <lsmsm0327@naver.com>
2025-05-22 02:24:20 +0900

    [refactor] #19 컬렉션뷰 레이아웃 수정

--- KB-STARBANKING/KB-STARBANKING/Presentation/InterestRate/InterestRateViewController.swift
+++ KB-STARBANKING/KB-STARBANKING/Presentation/InterestRate/InterestRateViewController.swift
@@ -15,29 +15,9 @@ class InterestRateViewController: UIViewController {
     private let scrollView = UIScrollView()
     private let contentView = UIView()
     
-    private let accountView = InfoView()
-    private let primeView = BasicRateView()
-    private lazy var interestRateCollectionView = RateViewController().createCollectionView()
-    
-    private let preferLabel = UILabel().then {
-        $0.text = "우대이율"
-        $0.font = .font(.title2_18_semibold)
-        $0.textColor = .kbBlack
-    }
-
-    private let cautionLabel = UILabel().then {
-        $0.text = "이율정보는 조회시점 현재를 기준으로 조회된 \n내용이며 만기 시에 최종 확정됩니다."
-        $0.font = .font(.body3_14_light)
-        $0.textColor = .gray6
-        $0.numberOfLines = 2
-    }
-
-    private let caution2Label = UILabel().then {
-        $0.text = "중도 해지할 경우 중도해지이율이 적용되므로 \n유의하시기 바랍니다. "
-        $0.font = .font(.body3_14_light)
-        $0.textColor = .gray6
-        $0.numberOfLines = 2
-    }
+    private let infoView = InfoView()
+    private let basicRateView = BasicRateView()
+    private let preferentialRateView = PreferentialRateView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,7 +35,7 @@ class InterestRateViewController: UIViewController {
     private func setUI() {
         view.addSubviews(closeHeaderView, scrollView)
         scrollView.addSubview(contentView)
-        contentView.addSubviews(accountView, primeView, preferLabel,interestRateCollectionView, cautionLabel, caution2Label)
+        contentView.addSubviews(infoView, basicRateView, preferentialRateView)
     }
     
     private func setLayout() {
@@ -74,39 +54,23 @@ class InterestRateViewController: UIViewController {
             $0.edges.width.equalToSuperview()
         }
         
-        accountView.snp.makeConstraints {
+        infoView.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.horizontalEdges.equalToSuperview()
-            $0.height.equalTo(200)
+            $0.height.equalTo(188)
         }
         
-        primeView.snp.makeConstraints {
-            $0.top.equalTo(accountView.snp.bottom).offset(46)
+        basicRateView.snp.makeConstraints {
+            $0.top.equalTo(infoView.snp.bottom).offset(46)
             $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(157)
         }
         
-        preferLabel.snp.makeConstraints {
-            $0.top.equalTo(primeView.snp.bottom).offset(46)
-            $0.leading.equalToSuperview().inset(23)
-        }
-
-        interestRateCollectionView.snp.makeConstraints {
-            $0.top.equalTo(preferLabel.snp.bottom).offset(17)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(212)
-        }
-        
-        cautionLabel.snp.makeConstraints {
-            $0.top.equalTo(interestRateCollectionView.snp.bottom).offset(22)
-            $0.leading.trailing.equalToSuperview().inset(40)
-        }
-        
-        caution2Label.snp.makeConstraints {
-            $0.top.equalTo(cautionLabel.snp.bottom).offset(15)
-            $0.leading.trailing.equalToSuperview().inset(40)
+        preferentialRateView.snp.makeConstraints {
+            $0.top.equalTo(basicRateView.snp.bottom).offset(46)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(360)
             $0.bottom.equalToSuperview().inset(40)
         }
     }
 }
-
--- KB-STARBANKING/KB-STARBANKING/Presentation/InterestRate/View/PreferentialRateView.swift
+++ KB-STARBANKING/KB-STARBANKING/Presentation/InterestRate/View/PreferentialRateView.swift
@@ -11,22 +11,43 @@ import SnapKit
 import Then
 
 class PreferentialRateView: UIView {
-
-    private lazy var interestRateCollectionView = RateViewController().createCollectionView()
+    
+    private var rateData = RateData.dummy()
     
     private let preferLabel = UILabel().then {
         $0.text = "우대이율"
         $0.font = .font(.title2_18_semibold)
         $0.textColor = .kbBlack
     }
-
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsHorizontalScrollIndicator = false
+        return collectionView
+    }()
+    
+    private let firstSquareView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray6
+        return view
+    }()
+    
     private let cautionLabel = UILabel().then {
         $0.text = "이율정보는 조회시점 현재를 기준으로 조회된 \n내용이며 만기 시에 최종 확정됩니다."
         $0.font = .font(.body3_14_light)
         $0.textColor = .gray6
         $0.numberOfLines = 2
     }
-
+    
+    private let secondSquareView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray6
+        return view
+    }()
+    
     private let caution2Label = UILabel().then {
         $0.text = "중도 해지할 경우 중도해지이율이 적용되므로 \n유의하시기 바랍니다. "
         $0.font = .font(.body3_14_light)
@@ -36,39 +57,96 @@ class PreferentialRateView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
         setUI()
         setLayout()
+        setCollectionView()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     private func setUI() {
-        addSubviews(preferLabel, interestRateCollectionView, cautionLabel, caution2Label)
+        addSubviews(preferLabel, collectionView, firstSquareView, cautionLabel, secondSquareView, caution2Label)
     }
-
+    
     private func setLayout() {
         preferLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(46)
-            $0.leading.equalToSuperview().inset(23)
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(23)
         }
-
-        interestRateCollectionView.snp.makeConstraints {
+        
+        collectionView.snp.makeConstraints {
             $0.top.equalTo(preferLabel.snp.bottom).offset(17)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(260)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(212)
+        }
+        
+        firstSquareView.snp.makeConstraints {
+            $0.top.equalTo(collectionView.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().offset(23)
+            $0.size.equalTo(3)
         }
         
         cautionLabel.snp.makeConstraints {
-            $0.top.equalTo(interestRateCollectionView.snp.bottom).offset(22)
-            $0.leading.trailing.equalToSuperview().inset(40)
+            $0.top.equalTo(collectionView.snp.bottom).offset(22)
+            $0.leading.equalTo(firstSquareView.snp.trailing).offset(8)
+        }
+        
+        secondSquareView.snp.makeConstraints {
+            $0.top.equalTo(firstSquareView.snp.bottom).offset(36)
+            $0.leading.equalToSuperview().offset(23)
+            $0.size.equalTo(3)
         }
         
         caution2Label.snp.makeConstraints {
-            $0.top.equalTo(cautionLabel.snp.bottom).offset(15)
-            $0.leading.trailing.equalToSuperview().inset(40)
-            $0.bottom.equalToSuperview().inset(40)
+            $0.top.equalTo(cautionLabel.snp.bottom).offset(7)
+            $0.leading.equalTo(secondSquareView.snp.trailing).offset(8)
         }
+    }
+    
+    private func setCollectionView() {
+        collectionView.register(RateViewCell.self, forCellWithReuseIdentifier: RateViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension PreferentialRateView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 160, height: 212)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 23, bottom: 0, right: 23)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 12
+    }
+}
+
+extension PreferentialRateView: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return rateData.count
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: RateViewCell.identifier,
+            for: indexPath
+        ) as? RateViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.dataBind(rateData[indexPath.row])
+        return cell
     }
 }
