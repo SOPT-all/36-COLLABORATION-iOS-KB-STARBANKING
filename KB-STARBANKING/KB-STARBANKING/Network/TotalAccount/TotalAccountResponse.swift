9bbb4ccad6a88b18ace3776705ac28dac77ac1de
hyeyeonie <shy020916@naver.com>
2025-05-23 13:39:49 +0900

    [network] #22 TotalAccountResponse 추가

--- KB-STARBANKING/KB-STARBANKING/Network/TotalAccount/TotalAccountResponse.swift
+++ KB-STARBANKING/KB-STARBANKING/Network/TotalAccount/TotalAccountResponse.swift
@@ -5,3 +5,25 @@
 //  Created by 이세민 on 5/22/25.
 //
 
+import Foundation
+
+struct TotalAccountResponse: Decodable {
+    let status: Int
+    let message: String
+    let data: TotalAccountData
+}
+
+struct TotalAccountData: Decodable {
+    let totalAccountBalance: Int
+    let accounts: [Account]
+}
+
+struct Account: Decodable {
+    let id: Int
+    let name: String
+    let accountNumber: String
+    let startDate: String
+    let endDate: String
+    let dDay: String
+    let accountBalance: Int
+}
